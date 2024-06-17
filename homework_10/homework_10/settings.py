62105cf575cb07dbe8ff498dd52e6725dcb9756e
BartoszKrus <150096024+BartoszKrus@users.noreply.github.com>
2024-06-17 16:28:21 +0200

    comments

--- homework_10/homework_10/settings.py
+++ homework_10/homework_10/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t4rf5k8p!uyj73_axf3y2n7&#gi$%@j3rh33xzw!o6n8i(h0o#'
+SECRET_KEY = os.getenv('DB_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
